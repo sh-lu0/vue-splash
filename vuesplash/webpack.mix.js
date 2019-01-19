5746c7a462eed2f0989064025457e5cfddf6753c
su_lu0 <29352194+sh-lu0@users.noreply.github.com>
2019-01-19 22:32:41 +0900

    HelloWorld

--- vuesplash/webpack.mix.js
+++ vuesplash/webpack.mix.js
@@ -1,16 +1,3 @@
-const mix = require('laravel-mix');
-
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
 const mix = require('laravel-mix')
 
 mix.browserSync('vuesplash.test')
